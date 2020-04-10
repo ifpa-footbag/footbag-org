902b70218c735a08ae8573a0fed5a75bec89b536
jsantt <35174533+jsantt@users.noreply.github.com>
2020-04-10 15:27:37 +0300

    Update news.js

--- news/news.js
+++ news/news.js
@@ -1,9 +1,6 @@
 /**
  * ADDING NEWS
  *
- * News article has to be wrapped inside <news-item> html element, which has to
- * contain header attribute.
- *
  * Simple example (with header and body):
  *
  * <news-item header="Footbag.org redesign">
@@ -25,13 +22,12 @@ const news = `
 <!-- ADD NEWS RIGHT BELOW THIS -->
 
 <news-item header="21st IFPA European Footbag Championships">
-We are excited to announce the
-21st European Footbag Championships 
-in Jaworzno, Poland from June 11-14, 2020 ! 
+  We are excited to announce the
+  21st European Footbag Championships 
+  in Jaworzno, Poland from June 11-14, 2020 ! 
 
-Paweł Nowak, Footbag Freestyle World Champion 2017 (Routines) and 2019 (Overall) in collaboration with Arthur Ledain and Kerstin Adineh from EFC (European Footbag Committee of IFPA) will host this year's miscellaneous event and invite you to the beautiful city of Jaworzno in the south of Poland! 
-
- </news-item>
+  Paweł Nowak, Footbag Freestyle World Champion 2017 (Routines) and 2019 (Overall) in collaboration with Arthur Ledain and Kerstin Adineh from EFC (European Footbag Committee of IFPA) will host this year's miscellaneous event and invite you to the beautiful city of Jaworzno in the south of Poland! 
+</news-item>
 
 <news-item header="Footbag.org redesign">
    Footbag.org has been redesigned
