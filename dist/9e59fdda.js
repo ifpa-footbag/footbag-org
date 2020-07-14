33c9f69fee4a0c98e25c99374e5d6c31b677866d
jsantt <35174533+jsantt@users.noreply.github.com>
2020-07-14 23:17:28 +0300

    remove extra dependencies from index.html

--- dist/9e59fdda.js
+++
@@ -1,9 +0,0 @@
-import"./efbcd7b9.js";import"./1e755928.js";import"./c5d84658.js";import{n as e}from"./9f63b991.js";
-
-class t extends HTMLElement{static get observedAttributes(){return["location"]}
-
-connectedCallback(){const t=`\n      <navigation-bar back></navigation-bar>\n        \n      <page-one-column>\n        <header class="header">\n          <h2>News</h2>\n        </header>\n        ${e}\n      </page-one-column>\n    `;this.attachShadow({mode:"open"}),this.shadowRoot.innerHTML=`\n    <style>\n      \n      \n    \n    </style>\n    ${t}\n    `,setTimeout(()=>{this._hideNews()},50)}
-
-attributeChangedCallback(e,t,n){console.log(e,t,n)}
-
-_hideNews(){this.shadowRoot.querySelector("page-one-column").querySelectorAll("news-item").forEach(e=>{e.getAttribute("header")===this.location.params.news||this.location.params.news==="all"?e.setAttribute("open",!0):e.style.display="none"})}}customElements.define("news-page",t);
