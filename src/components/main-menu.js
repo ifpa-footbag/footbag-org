bd686238bbd94487929a65290c0ca06129d1a2c4
Jani Säntti <jani.santti@gmail.com>
2020-08-02 10:28:12 +0300

    add menu

---
+++ src/components/main-menu.js
@@ -0,0 +1,87 @@
+import { angleDown, arrowRight } from './icons.js';
+
+class MainMenu extends HTMLElement {
+  _render() {
+    const style = `
+        button {
+            background: none;
+            border: none;
+            
+            color: var(--color-white);
+
+            font-size: var(--font-size-medium);
+            font-weight: var(--font-weight-bold);
+            padding: var(--space-xl) var(--space-m);
+            text-transform: uppercase;
+            white-space: nowrap;
+        }
+        svg {
+            fill: var(--color-white);
+        }
+
+        div {
+            background: var(--color-blue);
+            position: absolute;
+            right: 0;
+            padding: var(--space-xl);
+        }
+
+        a {
+            color: var(--color-white);
+            text-decoration: none;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        li {
+            padding: var(--space-m) 0;
+        }
+    `;
+
+    const html = `
+        <button aria-haspopup="true" aria-expanded="${this.open}">
+            menu&nbsp;<span aria-hidden="true">${angleDown}</span>
+        </button>
+        <div role="menu" ${this.open === true ? '' : 'hidden'}>
+            <ul>
+                <li><a href="/">${arrowRight} Home</a></li>
+                <li><a href="/getting-started">${arrowRight} Getting started</a></li>
+                <li><a href="http://footbag.org/reference/-/Footbag_Reference_Area">${arrowRight} Footbag disciplines</a></li>
+                <li><a href="http://footbag.org/rules/">${arrowRight} Rules</a></li>
+                <li><a href="http://footbag.org/faq/">${arrowRight} FAQ</a></li>
+                <li><a href="http://footbag.org/events/results">${arrowRight} Tournament results</a></li>
+                <li><a href="http://footbag.org/clubs/index">${arrowRight} Footbag clubs</a></li>
+                <li><a href="http://footbag.org/members/home">${arrowRight} Member area</a></li>
+                <li><a href="https://www.justgiving.com/internationalfootbagplayersassociationincorporated">${arrowRight} Donate</a></li>
+            </ul>
+        </div>
+    `;
+
+    this.shadowRoot.innerHTML = `
+    <style>
+        ${style}
+    </style>
+    ${html}
+    `;
+
+    this.shadowRoot.querySelector('button').addEventListener('click', () => {
+      this.open = !this.open;
+      this._render();
+    });
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.open = false;
+    this._render();
+  }
+}
+
+customElements.define('main-menu', MainMenu);
