8b2525884fcf61437e5046f3a247c637288d93d7
kalwakszymon <67378608+kalwakszymon@users.noreply.github.com>
2020-08-03 09:55:27 +0200

    Adding news (#24)
    * Adding a news article for Worlds Finals
    
    * Update news.js
    
    Co-authored-by: Jani Säntti <jani.santti@gmail.com>

--- src/news/news.js
+++ src/news/news.js
@@ -21,6 +21,39 @@
 const news = `
 <!-- ADD NEWS RIGHT BELOW THIS -->
 <news-item 
+  header="Online Freestyle WFC 2020 Finals results are here"
+  image="images/worlds2020.png"
+  long
+  short=""
+    date="June 2020">
+    <p>
+    The culminating point of this year's footbag season for Freestyle is now history. For the first time ever the IFPA organized 
+	an online-only competition with a live studio internet broadcast for the Finals Show. It was a massive success, gathering all 
+	the top players from all around the world who gave their best showcasing some unforgettable performances.
+	</p>
+	<p>
+	All results have been posted on the Footbag Reference, and can be found under the following link:
+	</p>
+	<p>
+	<a href="http://footbag.org/reference/-/IFPA_Online_Freestyle_Footbag_World_Championships_2020">http://footbag.org/reference/-/IFPA_Online_Freestyle_Footbag_World_Championships_2020</a>
+	</p>
+	<p>
+	The Shred30 World Record has been beaten, with a staggering 275.76 points run by Dominik "Nunchaku" Šimků. The previous best was 
+	274 points scored by Jan Weber at 2012 World Footbag Championships qualifications. Talk about cutting it close!
+	</p>
+	<p>
+	The Big Add Posse, a group of freestyle players pushing the limits of the sport has inducted a new member. This time the group honored a 
+	shredder from Poland - Rafał Kaleta, to be also knows as "Killa" from now on. Congratulations Rafał!
+	</p>
+	<p>
+	We want to thank all the competitors for showing that even in this difficult time, our sport can bring us all together. Congratulations 
+	to all the medalists and runner-ups. We hope that this event will bring an energy boost to all the members of our community to keep on 
+	playing and participating in events as it once more becomes possible.
+	</p>
+
+</news-item>
+
+<news-item 
   header="Online Freestyle WFC 2020 Finals start today!"
   image="images/worlds2020.png"
   long
@@ -86,63 +119,6 @@ const news = `
     <a href=”https://youtu.be/vPjK2fp1pkk”>here</a>. Have fun!</p>
 
 </news-item>
-
-<news-item 
-  header="21st IFPA European Footbag Championships"
-  image="images/euros-jaworzno.jpg"
-  short="We are excited to announce the 21st European Footbag Championships in Jaworzno, Poland from June 11-14, 2020! 
-  Paweł Nowak, Footbag Freestyle World Champion 2017 (Routines) and 2019 (Overall) in collaboration with Arthur Ledain 
-  and Kerstin Adineh from EFC (European Footbag Committee of IFPA) will host this year's miscellaneous event and 
-  invite you to the beautiful city of Jaworzno in the south of Poland!"
-  date="May 2020">
-    <p>
-      We are excited to announce the 21st European Footbag Championships in Jaworzno, Poland from June 11-14, 2020! 
-      Paweł Nowak, Footbag Freestyle World Champion 2017 (Routines) and 2019 (Overall) in collaboration with Arthur Ledain 
-      and Kerstin Adineh from EFC (European Footbag Committee of IFPA) will host this year's miscellaneous event and 
-      invite you to the beautiful city of Jaworzno in the south of Poland!</p>
-    <p>
-      Qualification rounds for all disciplines in Footbag Net and Freestyle will start in the morning of Thursday, June 11.
-      Finals will be held in front of the local public on Friday, June 12, at Jaworzno’s biggest shopping mall 
-      (some Freestyle disciplines) as well as on Saturday and Sunday, June 13-14, within the framework and in the area of
-      the Festival Days of the City Jaworzno at the City Stadium (Freestyle and Net).
-    </p> 
-    <p>
-      On the last day of the event Sunday, June 14, there will be some last finals at the city festival, played side by 
-      side with a freestyle, mini-net and workshop area for the public to try out and meet the players of the European championships. 
-      As an additional highlight, the hosts will show the footbag community some of the most beautiful landmarks of the 
-      Jaworzno region with footbag golf and chill&grill for all participants. 
-    </p>For the qualification days, there are two main event sites: a sports gym (6 footbag net courts plus additional freestyle area) 
-    and a shopping mall (for Freestyle events only). 
-    <hr>
-    <p>
-    Detailed information for players about schedule, accommodation, airport and local transfers, site maps and the
-    *official registration* (by early April) will be published and regularly updated at the event’s domain: 
-    footbag.online Come and join this event, which is again intended to be more than a tournament, shred and spike 
-    hard, hang out, celebrate our beloved sport and its community and spread the passion! 
-    </p>
-    <hr>
-     <p>
-      Background information Jaworzno is the 13th-largest city in Poland in terms of area, with almost 100,000 
-      population located in the south of Poland near Krakow and Katowice. The proximity of two airports and good 
-      road infrastructure means that commuting and arriving for all participants will be an easy matter.
-      A beautiful renovated town square and a church with one of the largest stained glass windows in Europe and 
-      are just a few reasons to visit this city! 
-    </p>
-      <p>
-      The festival City Days is a free event that provides the local public as well as tourists from everywhere 
-      in Poland and Europe with unlimited access to culture and entertainment, offering large-scale open air concerts,
-      fun sports events for children and adolescents and a great variety of food trucks. The festival is organized annually
-      to integrate the community, but also to remind about the history of a given region and cultivate traditions.
-      Many attractions and games have been prepared, so participants cannot complain of boredom. Some of the city's stunning
-      landmarks are Gródek Park, a unique place on the map of Poland. It delights with views and is not without exaggeration 
-      called by locals "Croatia" or "paradise island." and *GEOsfera, a theme park with gardens is a place open to visitors 
-      who are interested in history and excavation. This ecological and geological object is located in a former quarry. 
-    </p>
-    <p>
-       Walking around the area, you can come across life-size paleo-reconstructions of vertebrates unearthed there,
-       information boards and exhibitions, presenting and bringing closer the history, and more specifically the Triassic period.
-    </p>
- </news-item>
 
 <news-item 
   header="Footbag.org update in progress"
