fcaf46f8832333d03906db955dcf5db7ba534270
kalwakszymon <67378608+kalwakszymon@users.noreply.github.com>
2020-10-28 18:03:07 +0100

    Text update on getting started page (#36)
    * Text update on getting started page
    
    * prettier
    
    Co-authored-by: Jani Säntti <jani.santti@gmail.com>

--- src/getting-started-page.js
+++ src/getting-started-page.js
@@ -21,7 +21,7 @@ class GettingStartedPage extends HTMLElement {
 		  This guide will help you take your first step into this incredible world of 
 		  dexterity and skill.
         </p>
-		<h3>Choosing a footbag</h2>
+		<h3>Choosing a footbag</h3>
         <p>
           If you are just starting out, and want to learn basic kicks and tricks, an 8 panel 
 		  footbag will be the best choice. A local toy shop might have them in stock, and if 
@@ -29,7 +29,7 @@ class GettingStartedPage extends HTMLElement {
 		  should be perfectly fine! You can even make a bag yourself, a short tutorial for that can be 
 		  found <a href="https://www.youtube.com/watch?v=MUyzG4aEh_c">here</a>.
         </p>
-		<h3>Kicks and stalls</h2>
+		<h3>Kicks and stalls</h3>
 		<p>
 		  If you have your footbag, you are ready to start your journey. Start by watching a great tutorial 
 		  made by Franck Remy also known as FrankyStyle:
@@ -38,10 +38,21 @@ class GettingStartedPage extends HTMLElement {
     <youtube-video 
       src="https://www.youtube.com/embed/wkYxne1sq0Y"
       caption="Getting started tutorial"></youtube-video>
-		
-		<h3>Find a club near you</h2>
+		<h3>Footbag Freestyle and Footbag Net</h3>
+		<p>
+		While circle kicking is the way most people enter the sport, most players gravitate towards one of our main disciplines. You can learn more about Freestyle and Net on our site and decide for yourself if 
+		you want to specialize in one of those areas or play a little bit of everything. All that matters, is that you Keep Kicking!
+		</p>
+		<h3>Join us on Social Media</h3>
+		<p>
+		Our maing Facebook page can be found <a href="https://www.facebook.com/footbag.org">here</a>. We also have active groups for <a href="https://www.facebook.com/groups/footbag">Freestyle</a> and <a href="https://www.facebook.com/groups/154116001290293">Net</a> discussion.
+		We are also on Instagram, so follow <a href="https://www.instagram.com/footbag_sports/">footbag_sports</a>, and if you use Discord, an invite to our channel can be found under <a href="https://discord.com/invite/DAtQwG4">this link</a>.
+		</p>
+		<h3>Find a club near you</h3>
+		<p>
 		Want to find other people who play? Check the club listings at <a href="http://footbag.org/clubs/">http://footbag.org/clubs/</a> 
 		or start your own if you already have a group kicking together.
+		</p>
         </card-element>
       </page-one-column>
     `;
